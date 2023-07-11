4bbab3b0dd4991162edb9b14b9356f0ec763b22d
HLauxen <henrique.seefeld@gmail.com>
2023-07-10 21:14:11 -0300

    commit

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,8 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
-
-
 export async function POST(req: NextRequest) {
     const data = await req.json()
     console.log(data)
